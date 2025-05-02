5cc70a84633c136c257b87dce16458407921f1b2
vaishali4566 <vaishaliverma@globussoft.in>
2025-05-02 13:31:10 +0530

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import  { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import HomePage from './Pages/HomePage'
+import AboutPage from './Pages/AboutPage.jsx'
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>} />
+        <Route path="/about" element={<AboutPage/>} />
       </Routes>
     </BrowserRouter>
   )
