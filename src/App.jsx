f1f06f4e64828f82350ee14e0360a3870451d6ca
vishalglobussoftSDE <vishalvishwakarma@globussoft.in>
2025-05-02 13:08:16 +0530

    added display cart page and sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import  { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Sidebar from './components/Sidebar'
 import HomePage from './Pages/HomePage'
+import CartPage from './Pages/CartPage'
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>} />
+        <Route path="/cart" element={<CartPage/>} />
       </Routes>
     </BrowserRouter>
   )
