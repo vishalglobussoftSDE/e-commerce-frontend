06f634f0ddaa0029baa90c605738553c4c9dca2f
vishalglobussoftSDE <vishalvishwakarma@globussoft.in>
2025-05-02 10:57:00 +0530

    Added footer Component

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import Footer from '../components/Footer'
 
 const HomePage = () => {
   return (
     <div>
         <Sidebar/>
+        <Footer/>
     </div>
   )
 }
