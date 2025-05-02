0508ce55412d63582339df7f53be8930ea70dd98
vaishali4566 <vaishaliverma@globussoft.in>
2025-05-02 13:46:57 +0530

    added about router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import  { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
+import AboutPage from './Pages/AboutPage'
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>} />
+        <Route path="/about" element={<AboutPage/>} />
       </Routes>
     </BrowserRouter>
   )
