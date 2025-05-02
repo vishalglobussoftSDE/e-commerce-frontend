0a05acaf22c98d75b7f57e8ce7e3d5883aa680c2
anurag3311 <anuragverma@globussoft.in>
2025-05-02 13:40:55 +0530

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,26 +8,28 @@ const Header = () => {
     <div className='h-[140px] border-b-1 border-gray-300'>
         <div className='bg-black'>
             <div className='container grid grid-cols-3 h-[48px] content-center text-white text-[14px] font'>
-                <div className='col-span-2 align-text-right'><p className='float-right'>Summer sale For All Swim Suits And Free Express Delivery - OFF 50% ShopNow</p></div>
-                <div ><p className='float-right'>English</p></div>
+                <div className='col-span-2 align-text-right'><p className='float-right'>Summer sale For All Swim Suits And Free Express Delivery - OFF 50%! &nbsp;&nbsp;<a href='#' className='underline'>ShopNow</a></p></div>                
+                <select name='language' className='ml-auto pr-[4px] outline-none cursor-pointer'>
+                    <option value="english">English</option>                    
+                </select>
             </div>
         </div>
-        <div className='h-[92px] flex justify-center items-center'>
-            <div className='h-[38px] w-[1170px] flex items-center justify-between'>
-                <div className=''><h2 className='float-left'>Exclusive</h2></div>
+        <div className='container'>         
+            <div className='h-[38px] mt-[30px] flex items-center justify-between'>
+                <div className=''><h1 className='float-left font-bold text-2xl tracking-wide'>Exclusive</h1></div>
                 <div className='col-span-3 flex' >
                     <ul className='list-none mx-auto text-[16px]'>
-                        <a href='#'><li className='inline-block mx-4'>Home</li></a>
-                        <a href='#'><li className='inline-block mx-4'>Contact</li></a>
-                        <a href='#'><li className='inline-block mx-4'>About</li></a>
-                        <a href='#'><li className='inline-block mx-4'>Sign Up</li></a>
+                        <a href='#'><li className='inline-block mx-6'>Home</li></a>
+                        <a href='#'><li className='inline-block mx-6'>Contact</li></a>
+                        <a href='#'><li className='inline-block mx-6'>About</li></a>
+                        <a href='#'><li className='inline-block mx-6'>Sign Up</li></a>
                     </ul>
                 </div>
                 <div className='bg-gray-100 flex items-center h-[38px] p-[4px]'>
-                    <input type='text' placeholder='What are you looking for?' className='text-[12px]  h-[18px]'></input>
-                    <a className='w-[16px] h-[16px] m-[4px]'><CiSearch/></a>
+                    <input type='text' placeholder='What are you looking for?' className='text-[12px] px-[20px] h-[18px]'></input>
+                    <a className='w-[16px] h-[16px] m-[4px] cursor-pointer'><CiSearch/></a>
                 </div> 
-            </div>
+            </div>            
         </div>
     </div>
   )
