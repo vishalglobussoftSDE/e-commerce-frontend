c351b6fdb30d2c87816095882e5b1258f9707c52
vaishali4566 <vaishaliverma@globussoft.in>
2025-05-05 12:07:40 +0530

    updated home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,6 +4,10 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import OurProduct from "../components/HomePageComponents/OurProduct";
 import NewArrivals from "../components/HomePageComponents/NewArrivals";
+import AboutServices1 from "../components/AboutServices1";
+import servicesPng5 from "../assets/about-img/about-png5.png";
+import servicesPng6 from "../assets/about-img/about-png6.png";
+import servicesPng7 from "../assets/about-img/about-png7.png";
 
 const HomePage = () => {
   return (
@@ -12,8 +16,29 @@ const HomePage = () => {
       <main className="flex-grow mx-8 px-8">
         <Sidebar />
       </main>
-      <OurProduct/>
-      <NewArrivals/>
+      <div className="mx-14">
+        <OurProduct />
+        <NewArrivals />
+
+        <div className="flex justify-center gap-20 mb-34">
+          <AboutServices1
+            img={servicesPng5}
+            heading="FREE AND FAST DELIVERY"
+            text="Free delivery for all orders over $140"
+          />
+
+          <AboutServices1
+            img={servicesPng6}
+            heading="24/7 CUSTOMER SERVICE"
+            text="Friendly 24/7 customer support"
+          />
+          <AboutServices1
+            img={servicesPng7}
+            heading="MONEY BACK GUARANTEE"
+            text="We reurn money within 30 days"
+          />
+        </div>
+      </div>
       <Footer />
     </div>
   );
