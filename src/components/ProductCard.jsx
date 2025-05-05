32138bed955f215d3ff9b7d8a301535fe2db5f91
vishalglobussoftSDE <vishalvishwakarma@globussoft.in>
2025-05-05 11:36:07 +0530

    commit component/OurCardSection

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
-import card1 from '../assets/productCardImg/card1.png';
+import product1 from '../assets/productImg/product1.png';
+import ThreeStar from '../assets/ThreeStar.png';
 
-export const Product = () => {
+ const Product = () => {
   return (
     <div className='w-[270px] h-[320px] '>
      <div className='w-[270px] h-[250px] flex justify-center items-center bg-[#F5F5F5] rounded-md'>
-     <img src={card1} alt="" />
+     <img src={product1} alt="" />
      </div>
       <div >
         <p className='text-xl py-4'>Breed Dry Dog Food</p>
         <div className='flex gap-3.5'>
           <p className='text-red-700'>$100</p>
           <div className='flex'>
-            <img className='w-[20px] h-[20px]' src="/Vector.png" alt="" />
-            <img className='w-[20px] h-[20px]' src="/Vector.png" alt="" />
-            <img className='w-[20px] h-[20px]' src="/Vector.png" alt="" />
-            <img className='w-[20px] h-[20px]' src="/Vector.png" alt="" />
-            <img className='w-[20px] h-[20px]' src="/Vector.png" alt="" />
+            <img className='w-[100px] h-[20px]' src={ThreeStar} alt="" />
             </div>
           <p className='color'>(35)</p>
         </div>
@@ -24,3 +21,5 @@ export const Product = () => {
     </div>
   )
 }
+
+export default Product
