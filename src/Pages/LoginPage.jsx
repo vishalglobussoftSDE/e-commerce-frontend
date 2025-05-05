baf9fa69df0b1b33d8426a8ba1624b8c360bdb70
vaishali4566 <vaishaliverma@globussoft.in>
2025-05-05 10:29:09 +0530

    added home page imgs

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "../components/Header";
 import signupImg from "../assets/logSign-Img/log-sign.png";
 import { Link } from "react-router-dom";
-import googleIcon from "../assets/logSign-Img/googleIcon.png";
 import Footer from "../components/Footer.jsx";
 
 const LoginPage = () => {
