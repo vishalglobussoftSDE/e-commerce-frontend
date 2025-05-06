49f7fb7d2fad7c65339fc09346517f74378e8b24
anurag3311 <anuragverma@globussoft.in>
2025-05-06 10:18:56 +0530

    Added today's section of home component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AboutPage from './Pages/AboutPage'
 import AccountPage from './Pages/AccountPage'
 import ContactPage from './Pages/ContactPage'
 import CheckOut from './Pages/CheckOut'
+import Today from './components/Today'
 const App = () => {
   return (
     <BrowserRouter>
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/contact" element={<ContactPage/>} />
         <Route path="*" element={<NotFoundPage/>} />
         <Route path='/checkOut' element={<CheckOut/>} />
+        <Route path='/today' element={<Today/>} />
       </Routes>
     </BrowserRouter>
   )
