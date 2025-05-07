9a65e8d0100df3a596db28da446c8ac1323c9ac3
vishalglobussoftSDE <vishalvishwakarma@globussoft.in>
2025-05-07 10:46:46 +0530

    added logout in function in dropdown page

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { FaUser, FaBoxOpen, FaTimesCircle, FaStar, FaSignOutAlt } from "react-icons/fa";
-import {Link} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+
 const Dropdown = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+   
+    navigate("/login");
+  };
+
   return (
     <div className="absolute right-0 mt-2 w-60 bg-gradient-to-br from-gray-700 to-gray-900 text-white rounded-lg shadow-lg z-30 overflow-hidden">
       <ul className="py-2">
@@ -21,9 +33,12 @@ const Dropdown = () => {
           <FaStar />
           <span>My Reviews</span>
         </li>
-        <li className="flex items-center px-4 py-2 hover:bg-gray-800 cursor-pointer gap-3">
+        <li
+          onClick={handleLogout}
+          className="flex items-center px-4 py-2 hover:bg-gray-800 cursor-pointer gap-3"
+        >
           <FaSignOutAlt />
-          <Link to="/login">Logout</Link>
+          <span>Logout</span>
         </li>
       </ul>
     </div>
